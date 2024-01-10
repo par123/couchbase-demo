298b44c71f60a5d1d5c0a9672e0d461c3a3c319f
par123 <dharmparm474@gmail.com>
2024-01-10 18:30:28 +0530

    moving the change to repo

--- src/main/java/com/parmar/couchbase/couchbasedemo/model/Customer.java
+++ src/main/java/com/parmar/couchbase/couchbasedemo/model/Customer.java
@@ -1,11 +1,15 @@
 package com.parmar.couchbase.couchbasedemo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.core.mapping.Field;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document
 public class Customer {
 
